7d3180ae91be2da8ccd35697aec2a1460d21b76c
Gary Hobson <ugac007@gmail.com>
2017-11-20 15:00:56 +0100

    Update version.h
    adding min peer version for FORK1

--- src/version.h
+++ src/version.h
@@ -20,7 +20,8 @@ static const int GETHEADERS_VERSION = 31800;
 
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = GETHEADERS_VERSION;
-
+static const int MIN_PEER_PROTO_FORK1_VERSION = 250010;
+  
 //! nTime field added to CAddress, starting with this version;
 //! if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
